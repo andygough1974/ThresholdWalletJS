2aaf581d31eb734fa4d20a0847a4acf5bbd13ed4
mrorbita <mrorbita@gmail.com>
2015-03-21 02:12:46 +0400

    api mocks, stubs

--- Backend/controllers/default.js
+++ Backend/controllers/default.js
@@ -1,14 +1,21 @@
 exports.install = function() {
-	framework.route('/api/createWallet', mock, { flags : ['get', 'post'], timeout: 100000 });
-	framework.route('/api/sendMoney', mock, { flags : ['get', 'post'], timeout: 100000 });
-	framework.route('/api/getBalance', mock, { flags : ['get', 'post'], timeout: 100000 });
+	framework.route('/api/createWallet', createWallet, { flags : ['get', 'post'], timeout: 100000 });
+	framework.route('/api/sendMoney', sendMoney, { flags : ['get', 'post'], timeout: 100000 });
+	framework.route('/api/getBalance', getBalance, { flags : ['get', 'post'], timeout: 100000 });
 
 	//F.route('/', view_index);
     // or
     // F.route('/');
 };
 
-function mock() {
+function createWallet() {
+
+	// we could call any function from model like this
+	//MODEL('Alice')
+	// MODEL('Bob')
+
+	var address = '12AVMBVJDuDsijdTmjXqH39MgmrtmHhdre';
+	// just a mock
 	self.json(
 		{
 			result : true,
@@ -18,4 +25,33 @@ function mock() {
 			}
 		}
 	);
+}
+
+function sendMoney() {
+
+	// just a mock
+	self.json(
+		{
+			result : true,
+			data : {
+				address : address,
+				link : 'http://explorer.chain.com/addresses/' + address
+			}
+		}
+	);
+}
+
+function getBalance() {
+
+	// just a mock
+	self.json(
+		{
+			result : true,
+			data : {
+				address : address,
+				link : 'http://explorer.chain.com/addresses/' + address
+			}
+		}
+	);
+
 }
